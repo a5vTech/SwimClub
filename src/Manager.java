9d697ab49450950860e05efadc33d32c4cee8d85
a5vTech <a5vgaminghq@gmail.com>
2017-11-17 16:12:16 +0100

    Create member class

--- src/Manager.java
+++ src/Manager.java
@@ -1,6 +1,5 @@
 public class Manager extends Employee {
     //Fields
-    Boolean moustache = true;
     
     //Constructor
 
---
+++ src/Member.java
@@ -0,0 +1,10 @@
+public class Member {
+    String firstname;
+    String lastname;
+    String gender; //Maybe boolean instead ? and then if/else to set Male/Female
+    int age;
+    String Adress;
+    String email;
+    int phoneNumber;
+
+}
